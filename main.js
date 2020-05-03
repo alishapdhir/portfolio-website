6b360a6a4a2e2eb74f077fae84c9ab8167500b1d
Alisha Dhir <alishadhir@Second-Gina.fios-router.home>
2020-05-03 14:25:04 -0400

    YES ITS WORKING

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-new CircleType(document.getElementById('namecircle'));
+// const circleType = new CircleType(document.getElementById('#namecircle'));
+// circleType.radius(200).dir(-1);
+// WHY CANT I FIGURE OUT NAMECIRCLE.JS THE INTERNET IS BEING UNHELPFUL
